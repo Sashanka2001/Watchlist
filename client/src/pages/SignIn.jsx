7589bf25675708843b4707a15beef3e4fd17916c
Sashanka2001 <chamudisashanka01@gmail.com>
2026-01-05 10:59:20 +0530

    Remove unused Lock import from SignIn component for cleaner code

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { Lock, Mail } from "lucide-react";
 import { LogIn, Mail, Lock, AlertCircle, Eye, EyeOff } from "lucide-react";
 
 export default function SignIn() {
